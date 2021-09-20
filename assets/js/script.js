ec6f49922326dbe57572ed917bbd64949a02e7e1
Jeff Anderson-Lester <viperhq@hotmail.com>
2021-09-20 14:35:32 -0500

    finished save feature for notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,5 +39,17 @@ $(document).ready(function () {
     });
   }
 
+  // Load items from each hour block and from the .description class from local storage if previously stored.
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+
   timeTracker();
 });
