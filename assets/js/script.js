63855054c9ea829fd539fda27767fd413d7eb83e
Jeff Anderson-Lester <viperhq@hotmail.com>
2021-09-20 14:29:04 -0500

    Added color change elemtns to time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
-"use strict";
-
 //Moment.js used to display today's date and time:
-var dateNow = moment().format("dddd, MMM Do YYYY");
+let todayDate = moment().format("dddd, MMM Do YYYY");
 $("#currentDay").html(todayDate);
 
 $(document).ready(function () {
@@ -9,9 +7,37 @@ $(document).ready(function () {
   $(".saveBtn").on("click", function () {
     //Get descriptions from jQuery
     let text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+    let time = $(this).parent().attr("id");
 
     //Save text area to Local Storage:
     localStorage.setItem(time, text);
   });
+
+  //Time tracker feature
+  function timeTracker() {
+    let currentTime = moment().hour();
+
+    //Looping over the .timeblocks class the rows
+
+    $(".time-block").each(function () {
+      let hour = parseInt($(this).attr("id").split("hour")[1]);
+
+      //Checks the time and adds/removes classes for the backgrounds for those classes
+      if (hour < currentTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      } else if (hour === currentTime) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  timeTracker();
 });
